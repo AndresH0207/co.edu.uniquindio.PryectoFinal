0e759403babde650227bb1aa75099f4fb141983d
mmaykol <32054958+mmaykol@users.noreply.github.com>
2021-12-06 00:24:23 -0500

    version 1.7 se agregan vistas.

---
+++ src/co/edu/uniquindio/proyectoFinal/controller/RedVendedoresController.java
@@ -0,0 +1,46 @@
+package co.edu.uniquindio.proyectoFinal.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class RedVendedoresController {
+
+
+
+	
+
+	    @FXML
+	    private ResourceBundle resources;
+
+	    @FXML
+	    private URL location;
+
+	    @FXML
+	    private TableView<?> TableViewRed;
+
+	    @FXML
+	    private TableColumn<?, ?> tbRedNombre;
+
+	    @FXML
+	    private TableColumn<?, ?> tbRedApellido;
+
+	    @FXML
+	    private TableColumn<?, ?> tbRedCedula;
+
+	    @FXML
+	    private TableColumn<?, ?> tbRedDirecion;
+
+	    @FXML
+	    void initialize() {
+	        
+
+	    }
+	}
+
+	
+	
+	
+
